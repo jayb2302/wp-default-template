6e7534de88ba24961f62099589f54cc64a6bdf3f
jayb2302 <jonbja01@easv365.dk>
2023-02-22 10:45:48 +0100

    okei

--- single.php
+++ single.php
@@ -5,14 +5,19 @@
  */
 get_header();
 ?>
+<!-- #primary.content-area -->
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
         <?php 
         while( have_posts()) :
             the_post();
-            the_title();
-            the_content();
+            // the_title();
+            // the_content();
+            get_template_part( 'template-parts/post/content', get_post_format(  ) );
         endwhile;
+        // if comments are open then we show the comments template
+        if(comments_open()|| get_comments_number( ));
+        endif;
         ?>
     </main>
 </div>
---
+++ template-parts/page/content.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * 
+ * 
+ */
+?>
+<article id="post-<?php the_ID(  ); ?>">
+    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+    <?php  
+        if (has_post_thumbnail( )):
+            the_post_thumbnail( 'full' );
+        endif;
+
+    ?>
+    <div class="entry-content">
+    
+
+        <?php the_content(  ); array(
+        'before' => '<div class="page-links">' . esc_html( 
+        'Pages','wp-template-default'
+        ),
+        'after' => '</div>',)
+        ?>
+    </div>
+
+    <?php if(get_edit_post_link( )): ?>
+    
+        <footer class="entry-footer">
+            <?php 
+                edit_post_link( 
+                    sprintf(
+                        wp_kses(
+                            __( 'Edit <span class="screen-reader-text">%s</span>'),
+                                array(
+                                    'span' => array(
+                                    'class' => array(),
+                                )
+                            )
+                                    ),
+                        get_the_title()
+                    ),
+                        '<span class="edit-link">',
+                        '</span>'
+                );
+            ?>
+        </footer>
+        <?php endif; ?>
+
+</article>
