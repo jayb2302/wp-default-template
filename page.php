341ec01a9249cf2f8af0644934fe9c65a61bd44f
jayb2302 <jonbja01@easv365.dk>
2023-03-09 12:02:13 +0100

    ok

--- page.php
+++ page.php
@@ -1,33 +1,29 @@
 <?php
 /**
- * Template for displaying pages
+ * template for displaying all pages
  */
 get_header();
 ?>
 
 <!-- #primary.content-area -->
 <div id="primary" class="content-area">
-    <main id="id" class="site-main">
-    <?php
-       
-
-        while ( have_posts() ) :
-          the_post();
-          get_template_part( 'template-parts/page/content', 'page' );  // content-page.php  (the 'page' is the slug, the 'content' is the prefix)
-  
-          // second part
-          // If comments are open then we can show the comments template.
-          if ( comments_open() || get_comments_number() ) :
-            comments_template();
-          endif;
-        endwhile;
+  <main id="id" class="site-main">
+    <?php 
+      while ( have_posts() ) :
+        the_post();
+        get_template_part( 'template-parts/page/content', 'page' );  // content-page.php  (the 'page' is the slug, the 'content' is the prefix)
       
+        // second part
+        // If comments are open then we can show the comments template.
+        if ( comments_open() || get_comments_number() ) :
+          comments_template();
+        endif;
+      endwhile;
     ?>
-
-    </main>
-    <?php
-    get_sidebar( )
-    ?>
+  </main>
+  <?php 
+    get_sidebar();
+  ?>
 </div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,24 +1,26 @@
-<?php 
+<?php
 /**
- * Single page template
- * 
+ * Singple Page template
  */
 get_header();
 ?>
 <!-- #primary.content-area -->
 <div id="primary" class="content-area">
-    <main id="main" class="site-main">
-        <?php 
-        while( have_posts()) :
-            the_post();
-            // the_title();
-            // the_content();
-            get_template_part( 'template-parts/post/content', get_post_format(  ) );
-        endwhile;
-        // if comments are open then we show the comments template
-        if(comments_open() || get_comments_number( )) :
+  <!-- main#main.site-main -->
+  <main id="main" class="site-main">
+    <?php
+      while( have_posts()) :
+        the_post();
+        //the_title('<h1>','</h1>' );
+        //the_content();
+        get_template_part( 'template-parts/post/content', get_post_format() );
+      endwhile;
+
+      // if comments are open then we show the comments template
+      if( comments_open() || get_comments_number() ) :
         comments_template();
-        endif;
-        ?>
-    </main>
+      endif;
+    ?>
+    
+  </main>
 </div>
